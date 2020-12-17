d4d6cbd1fea4d6c0766faffff0c57b645bf264c6
FieryInferno <bagassetia271@gmail.com>
2020-12-17 17:10:05 +0700

    Perbaikan tombol simpan RBA 221

--- resources/views/admin/rba/rba221/create.blade.php
+++ resources/views/admin/rba/rba221/create.blade.php
@@ -269,7 +269,7 @@
         </div>
         <div class="modal-footer bg-whitesmoke br">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-          <button type="button" class="btn btn-primary">Simpan</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Simpan</button>
         </div>
       </form>
     </div>
